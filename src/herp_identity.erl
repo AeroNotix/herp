fbb206e515e77c6a735be18c2b521167eeabcb4f
Aaron <aaron.l.france@gmail.com>
2013-05-26 18:13:15 +0200

    Re-organized the whole project to OTP standards.

---
+++ src/herp_identity.erl
@@ -0,0 +1,28 @@
+-module(herp_identity).
+-compile(export_all).
+-define(REGION_URL, "https://region-b.geo-1.identity.hpcloudsvc.com:35357/v2.0/").
+
+
+login(Username, Password, TenantID) ->
+	Body = create_auth_body(Username, Password, TenantID),
+	{ok, {{_HTTP, Status, _Msg}, Headers, Resp}} = httpc:request(post, {string:concat(?REGION_URL, "tokens"),
+																["accept", "application/json"],
+																"application/json", Body}, [], []),
+	case Status of
+		200 ->
+			io:format(jsx:decode(list_to_binary(Resp)));
+		Else ->
+			io:format(Else)
+	end.
+			
+
+create_auth_body(Username, Password, TenantID) ->
+	U = list_to_binary(Username),
+	P = list_to_binary(Password),
+	T = list_to_binary(TenantID),
+	Body = [{<<"auth">>,
+			 [{<<"passwordCredentials">>,
+			   [{<<"username">>, U},
+				{<<"password">>, P}]},
+			  {<<"tenantId">>, T}]}],
+	jsx:encode(Body).
