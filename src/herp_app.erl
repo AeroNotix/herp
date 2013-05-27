21d43d3f56e54db5b7f00b6c22cdb2d8a5141179
AeroNotix <aaron.l.france@gmail.com>
2013-05-27 12:11:34 +0200

    Moving to a supervisor tree.

---
+++ src/herp_app.erl
@@ -0,0 +1,25 @@
+-module(herp_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    case application:get_env(has_proxy) of
+        true ->
+            ProxyAddress = application:get_env(proxyaddr),
+            ProxyPort = application:get_env(proxyport),
+            httpc:set_options([{proxy, {{ProxyAddress, ProxyPort},
+                                        ["localhost"]}}]);
+        false ->
+            ok
+    end,
+    herp_sup:start_link().
+
+stop(_State) ->
+    ok.
