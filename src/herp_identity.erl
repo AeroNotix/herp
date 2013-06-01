a2ec6c385ab7d9de3b77302da0baf7cf462c8b61
Aaron <aaron.l.france@gmail.com>
2013-06-01 18:10:02 +0200

    Documented th herp_identity. folder.

--- src/herp_identity.erl
+++ src/herp_identity.erl
@@ -4,6 +4,12 @@
 %% API
 -export([login/3, login_conf/0]).
 
+%% @doc Logs in and returns a Client for which we can use for futher
+%% service calls.
+%% @spec login(Username::string(), Password::string(), TenantID::string) ->
+%%      {ok, Pid}
+%% where
+%%   Pid = pid()
 login(Username, Password, TenantID) ->
 	Body = create_auth_body(Username, Password, TenantID),
 	{ok, {{_HTTP, Status, _Msg}, _Headers, Resp}} = httpc:request(post, {string:concat(?REGION_URL, "tokens"),
@@ -17,12 +23,20 @@ login(Username, Password, TenantID) ->
 		  end,
 	Pid.
 
+%% @doc login_conf uses the login details supplied in the sys.config
+%% file and calls login/3 with those details.
+%% @spec login_conf() -> {ok, Pid}
+%% where
+%%   Pid = pid()
 login_conf() ->
     {ok, Username} = application:get_env(herp, username),
     {ok, Password} = application:get_env(herp, password),
     {ok, TenantID} = application:get_env(herp, tenant_id),
     login(Username, Password, TenantID).
 
+%% @doc create_auth_body is a helper which interpolates the login
+%% details with a JSON body.
+%% @private
 create_auth_body(Username, Password, TenantID) ->
 	U = list_to_binary(Username),
 	P = list_to_binary(Password),
