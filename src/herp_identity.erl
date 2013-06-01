7f59474285e74db63e2d5b2b5ab91fc10d080a21
Aaron <aaron.l.france@gmail.com>
2013-06-01 18:09:48 +0200

    Properly label our exports instead of export_all.@

--- src/herp_identity.erl
+++ src/herp_identity.erl
@@ -1,7 +1,8 @@
 -module(herp_identity).
--compile(export_all).
 -define(REGION_URL, "https://region-b.geo-1.identity.hpcloudsvc.com:35357/v2.0/").
 
+%% API
+-export([login/3, login_conf/0]).
 
 login(Username, Password, TenantID) ->
 	Body = create_auth_body(Username, Password, TenantID),
