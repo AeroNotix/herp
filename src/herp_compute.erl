1530d5ca2f9364701cbb4f058ae9a047eb1fb811
AeroNotix <aaron.l.france@gmail.com>
2013-06-06 10:58:37 +0100

    Make checks for the specific optoins which the compute CreateServer endpoint requires.

--- src/herp_compute.erl
+++ src/herp_compute.erl
@@ -3,7 +3,24 @@
 -export([create_server/2]).
 
 create_server(ClientRef, ServerProp) when is_list(ServerProp) ->
-    S = [{<<"server">>, ServerProp}],
-    io:format("~p~n", [S]),
-    ServerEncoded = jsx:encode(S),
-    gen_server:call(herp_refreg:lookup(ClientRef), {create_server, ServerEncoded}).
+    Name = proplists:get_value(<<"name">>, ServerProp),
+    Flavor = proplists:get_value(<<"flavorRef">>, ServerProp),
+    ImageRef = proplists:get_value(<<"imageRef">>, ServerProp),
+    case verify_compute_request([Name, Flavor, ImageRef]) of
+        ok ->
+            S = [{<<"server">>, ServerProp}],
+            ServerEncoded = jsx:encode(S),
+            gen_server:call(herp_refreg:lookup(ClientRef), {create_server, ServerEncoded});
+        {error, Field} ->
+            {error, {Field, missing}}
+    end.
+
+verify_compute_request([]) ->
+    ok;
+verify_compute_request([H|T]) ->
+    case H of
+        undefined ->
+            {error, H};
+        _Else ->
+            verify_compute_request(T)
+    end.
